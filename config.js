0e3687f9113728cae22b26f88937f4f9ebe7c776
THAWATCHAI FHAIPOLSAN <sector105@gmail.com>
2020-03-06 19:05:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     name : 'NanoBOT',
     platform : 'arduino-avr',
     title : 'NanoBOT',
-    description : 'Arduino NANO Robot',
+    description : 'NanoBOT บอร์ดเรียนรู้สำหรับสร้างหุ่นยนต์ และระบบอัตโนมัติ',
     author : 'Home IT Maker',
     website : 'http://www.itmaker.co',
     email : 'sector105@gmail.com',
